80f1e38c11359ff1a894bdb7c99b27c5435e5bab
YuudaiHiiro <jamroack@gmail.com>
2026-01-25 11:27:50 +0200

    Add ad-hoc signing and increase auto-unlock timeout to 8 minutes

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -5,12 +5,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var mainController: MainViewController!
     
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // Check if app is quarantined or in a problematic location
-        if isAppInProblematicLocation() {
-            showInstallationDialog()
-            return
-        }
-        
         // Request accessibility permissions
         checkAccessibilityPermissions()
         
@@ -56,70 +50,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Make window float above other windows
         window.level = .floating
         window.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
-    }
-    
-    private func isAppInProblematicLocation() -> Bool {
-        let bundlePath = Bundle.main.bundlePath
-        
-        // Check if running from DMG or quarantined location
-        if bundlePath.contains("/Volumes/") || 
-           bundlePath.contains("/AppTranslocation/") ||
-           bundlePath.contains(".dmg") {
-            return true
-        }
-        
-        // Check for quarantine attribute using xattr
-        let task = Process()
-        task.launchPath = "/usr/bin/xattr"
-        task.arguments = ["-p", "com.apple.quarantine", bundlePath]
-        
-        let pipe = Pipe()
-        task.standardOutput = pipe
-        task.standardError = pipe
-        
-        do {
-            try task.run()
-            task.waitUntilExit()
-            
-            // If exit code is 0, quarantine attribute exists
-            if task.terminationStatus == 0 {
-                return true
-            }
-        } catch {
-            // If we can't check, assume it's okay
-        }
-        
-        return false
-    }
-    
-    private func showInstallationDialog() {
-        let alert = NSAlert()
-        alert.messageText = "Please Install ShinyMac"
-        alert.informativeText = """
-        ShinyMac needs to be installed in your Applications folder to work properly.
-        
-        Please:
-        1. Drag ShinyMac to your Applications folder
-        2. Launch it from there
-        
-        (Running from a disk image or Downloads folder may cause issues)
-        """
-        alert.alertStyle = .informational
-        alert.addButton(withTitle: "Open Applications Folder")
-        alert.addButton(withTitle: "Continue Anyway")
-        alert.addButton(withTitle: "Quit")
-        
-        let response = alert.runModal()
-        if response == .alertFirstButtonReturn {
-            // Open Applications folder
-            NSWorkspace.shared.selectFile(nil, inFileViewerRootedAtPath: "/Applications")
-            NSApp.terminate(nil)
-        } else if response == .alertSecondButtonReturn {
-            // User wants to continue anyway - let them
-            return
-        } else {
-            NSApp.terminate(nil)
-        }
     }
     
     private func checkAccessibilityPermissions() {
